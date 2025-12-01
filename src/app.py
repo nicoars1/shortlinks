d6c92f2dd6cb5f13908fc969a1024708759cd3fa
nicoars1 <nicolas.arsegot.gustavo@gmail.com>
2025-11-30 22:16:40 -0300

    Refactor database path configuration, update CSS formatting, and link custom stylesheet in index.html

--- src/app.py
+++ src/app.py
@@ -1,10 +1,14 @@
 from flask import Flask, jsonify, render_template
 from models import db, Links
 from main_routes import bp
+import os
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+DB_PATH = os.path.join(BASE_DIR, "..", "database", "links.db")
 
 
 app = Flask(__name__, template_folder="templates", static_folder="static")
-app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///C:/Users/nicol/Desktop/shortUrl/database/links.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{DB_PATH}"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db.init_app(app)
 
